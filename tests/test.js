fdbe788a3ae9cdf780c756ce3c12922acde5d5a7
jesobreira <jefrey@forteras.tech>
2018-02-16 14:24:47 -0200

    fix require

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,7 @@
 const username = "your-username-here";
 const password = "your-password-here";
 
-const zenvia = require('zenvia-sms')(username, password, "debug"); // remove "debug" in production
+const zenvia = require('node-zenvia-sms')(username, password, "debug"); // remove "debug" in production
 
 zenvia.sendSms("test", "remetente", "5511987654321", "", "", "", "NONE", function success() {
 	console.log("SMS was sent");
